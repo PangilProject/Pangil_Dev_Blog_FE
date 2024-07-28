5f4de6a14ef02276f3fc8a133de204856b9d4467
pangil5634 <oksk6681@naver.com>
2024-07-29 04:44:17 +0900

    edit : Router Several path

--- src/Router.js
+++ src/Router.js
@@ -4,15 +4,22 @@ import MainPage from "../src/Pages/MainPage/MainPage";
 import ShowPage from "../src/Pages/ShowPage/ShowPage";
 import WritePage from "../src/Pages/WritePage/WritePage";
 import LoginPage from "./Pages/LoginPage/LoginPage";
+import Layout from "./Layout/Layout";
+import NotFoundPage from "./Pages/NotFoundPage";
 
 function Router() {
 
     return (
         <Routes>
-            <Route path="/" element={<MainPage />}/>
-            <Route path="/show" element={<ShowPage />}/>
-            <Route path="/write" element={<WritePage />}/>
-            <Route path="/login" element={<LoginPage/>}/>
+            <Route element={<Layout/>}>
+
+                <Route path="/" element={<MainPage />}/>
+                <Route path="/show/:id" element={<ShowPage />}/> {/* 동적 경로 */}
+                <Route path="/write" element={<WritePage />}/>
+                <Route path="/login" element={<LoginPage/>}/>
+            </Route>
+            <Route path="*" element={<NotFoundPage />}/>
+
         </Routes>
     );
 }
