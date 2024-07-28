fb5a94cb09d4c5be2006765764111f07a2ba0310
pangil5634 <oksk6681@naver.com>
2024-07-29 04:42:12 +0900

    feat : MainPage 초안

---
+++ src/Pages/MainPage/Components/PostCard.js
@@ -0,0 +1,80 @@
+import {useState} from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const PostCard = (e) => {
+    const [sendPostData, setSendPostData] = useState(e.postData || {
+        title: "",
+        content: ""
+    });
+    return (
+        <PostCardContainer>
+            <PostTitle to={`/show/${sendPostData.id}`}>
+                {sendPostData.title}
+            </PostTitle>
+            <PostContent>
+                {sendPostData.content}
+            </PostContent>
+            <RowBox>
+                <PostDate>2024.08.25</PostDate>
+                <PostCategory>개발</PostCategory>
+            </RowBox>
+        </PostCardContainer>
+    );
+}
+
+const PostCardContainer = styled.div `
+    width: 100%;
+    height : 200px;
+    color : ${props => props.theme.colors.one};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border-bottom : 2px solid ${props => props.theme.colors.one};
+
+`;
+
+const PostTitle = styled(Link) `
+
+    font-size: 25px;
+    font-weight: 600;
+
+    margin-top: 15px;
+    color : ${props => props.theme.colors.one};
+    text-decoration: none;
+
+    transition: color 0.5s ease; /* color 속성에 애니메이션 추가 */
+
+    &:hover {
+        color: black; /* hover 상태에서의 색상 */
+    }
+`;
+
+const PostContent = styled.div `
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    max-width: 100%;
+
+    font-size: 17px;
+
+    margin-top: 5px;
+`;
+const RowBox = styled.div `
+
+    display: flex;
+
+    font-size: 15px;
+
+    margin-top: 5px;
+
+`
+const PostDate = styled.div `
+
+`;
+const PostCategory = styled.div `
+
+`;
+export default PostCard;
--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -1,10 +1,60 @@
+import {useEffect, useState} from "react";
+import {getPostData} from "../../API/postAPI";
+import {Link} from "react-router-dom";
+import styled from "styled-components";
+import PostCard from "./Components/PostCard";
+import {PageContainer} from "../../Layout/Common";
+import {PageTitleBox} from "../../Layout/PageTitle";
+
 function MainPage() {
-    
+    const [postDataList, setPostDataList] = useState([]);
+    const [newPost, setNewPost] = useState({title: '', content: ''});
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await getPostData();
+            setPostDataList(response);
+        }
+        getData();
+    }, []);
+
     return (
-        <div>
-            MainPage
-        </div>
+        <PageContainer>
+            <PageTitleBox
+                pageTitle="전체 글"
+                pageDescription="팡팡 터지는 팡일처럼 개발도 팡팡 터지는 시원한 느낌으로 하고 싶은 개발자입니다."/>
+
+            <PostListContainer>
+                {
+                    postDataList.map(
+                        (postData, index) => (<PostCard key={index} postData={postData}/>)
+                    )
+                }
+            </PostListContainer>
+            <WritingButton to="/write">글 쓰러 가기</WritingButton>
+        </PageContainer>
     );
 }
+
+export const PostListContainer = styled.div `
+    width: 1000px;
+    height : 2000px;
+    /* background-color: ${props => props.theme.colors.five}; */
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (max-width : 1300px) {
+        width: 100%;
+        padding : 0% 10%;
+        box-sizing: border-box;
+        
+    }
+`;
+
+const WritingButton = styled(Link)`
+    width: 300px;
+    color : white;
+`;
 
 export default MainPage;
