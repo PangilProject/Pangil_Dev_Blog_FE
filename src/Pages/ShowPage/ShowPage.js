854e5357e990dbbd89bfaa2a6a1fc425e7d95630
pangil5634 <oksk6681@naver.com>
2024-07-29 19:36:00 +0900

    feat : ShowPage

--- src/Pages/ShowPage/ShowPage.js
+++ src/Pages/ShowPage/ShowPage.js
@@ -1,35 +1,69 @@
-import {useParams} from "react-router-dom";
+import {Link, useNavigate, useParams} from "react-router-dom";
 import {PageContainer} from "../../Layout/Common";
 import {PageTitleBox} from "../../Layout/PageTitle";
 import {useEffect, useState} from "react";
-import {getPostDataOne} from "../../API/postAPI";
+import {deletePostData, getPostDataOne} from "../../API/postAPI";
 import styled from "styled-components";
-import { PostListContainer } from "../MainPage/MainPage";
+import {PostListContainer} from "../MainPage/MainPage";
 
 function ShowPage() {
-    // useParams 훅을 사용하여 URL에서 id를 추출합니다.
     const {id} = useParams();
-    const [postData, setPostData] = useState({title: "", content: ""})
+    const [postData, setPostData] = useState({title: "", content: ""});
+    const navigate = useNavigate();
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         const getData = async () => {
-            const response = await getPostDataOne(id);
-            setPostData(response);
-        }
+            try {
+                const response = await getPostDataOne(id);
+                console.log(response);
+
+                if (!response || response.status === 404) {
+                    setPostData({title: "없는 게시물입니다.", content: ""});
+                    setError(true);
+                } else {
+                    setPostData(response);
+                    setError(false);
+                }
+            } catch (error) {
+                if (error.response && error.response.status === 404) {
+                    setPostData({title: "없는 게시물입니다.", content: ""});
+                } else {
+                    setPostData({title: "오류가 발생했습니다.", content: "다시 시도해주세요."});
+                }
+                setError(true);
+            }
+        };
         getData();
-    }, [id]); // id를 의존성 배열에 추가
+    }, [id]);
+
+    const handleDeletePost = async () => {
+        const deleteData = async () => {
+            await deletePostData(id);
+        };
+        const result = window.prompt("관리자 비밀번호를 입력해주세요.");
+        if (result === "pangil") {
+            if (window.confirm("삭제하시겠습니까?")) {
+                await deleteData();
+                alert("삭제 되었습니다.");
+                navigate("/");
+            }
+        }
+    };
 
     return (
         <PageContainer>
-            <PageTitleBox pageTitle={`게시물 ${postData.title}`}
-                // pageDescription={`팡팡 터지는 팡일처럼 개발도 팡팡 터지는 시원한 느낌으로 하고 싶은 개발자입니다.`}
-            />
+            <PageTitleBox pageTitle={`게시물 ${postData.title}`}/>
             <PostListContainer>
                 <PageContent>
                     {postData.content}
                 </PageContent>
+                {
+                    error === false
+                        ? <DeleteButton onClick={handleDeletePost}>삭제하기</DeleteButton>
+                        : <BackHome to="/">Home으로 돌아가기</BackHome>
+                }
             </PostListContainer>
-
         </PageContainer>
     );
 }
@@ -38,5 +72,42 @@ const PageContent = styled.div `
     font-size: 17px;
     margin-top: 50px;
     line-height: 35px;
-`
+`;
+
+const DeleteButton = styled.button `
+    width: 100px;
+    height: 30px;
+    font-size: 17px;
+    font-weight: bold;
+    border: none;
+    transition: color 1s ease, background-color 1s ease; /* color 속성에 애니메이션 추가 */
+    border-radius: 5px;
+
+    &:hover {
+        background-color: black;
+        color: white;
+    }
+`;
+
+const BackHome = styled(Link)`
+    width: 180px;
+    height: 30px;
+    font-size: 17px;
+    font-weight: bold;
+    border: none;
+    transition: color 1s ease, background-color 1s ease; /* color 속성에 애니메이션 추가 */
+    border-radius: 5px;
+    text-decoration: none;
+    color : black;
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:hover {
+        background-color: black;
+        color: white;
+    }
+`;
+
 export default ShowPage;
